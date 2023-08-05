868315e3ea4ff2b642d1148d744ab7889f4eb9ff
Poudel0 <poudel0sandesh@gmail.com>
2023-08-05 06:03:12 +0545

    Added Scripts Build react app Serving the app

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -11,6 +12,7 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
 
 module.exports = app;
--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,5 +1,57 @@
-const planets = [];
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const path = require("path");
 
-module.exports = { planets };
+let habitablePlanets = [];
+// const habitablePlanets
+function isHabitable(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
 
-// retrieve planets from the data from csv
+function loadPlanets() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "keplar_data.csv")
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true, // Returns each row in csv as js object as key-value instead of just arrays of values in a row
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitable(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on("end", () => {
+        // console.log(
+        //   habitablePlanets.map((planet) => {
+        //     return planet["kepler_name"];
+        //   })
+        // );
+        console.log(
+          `${habitablePlanets.length} Potential habitable planets have been discovered so far !`
+        );
+        resolve();
+      })
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+}
+
+function getHabitablePlanets() {
+  return [...habitablePlanets];
+}
+
+module.exports = {
+  loadPlanets,
+  planets: habitablePlanets,
+};
